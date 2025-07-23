12a4377111b51bee06f3bae3490142f46525500c
Sergei P <primgres@MacBook-Air-Sergei.local>
2025-07-23 12:52:26 +0300

    add time-to-live for cache

--- cache.go
+++ cache.go
@@ -1,21 +1,39 @@
 package cache
 
+import "time"
+
+type Value struct {
+	value      any
+	ttl        time.Duration
+	expiration time.Time
+}
+
 type Cache struct {
-	data map[string]any
+	data map[string]Value
 }
 
 func New() *Cache {
 	return &Cache{
-		data: make(map[string]interface{}),
+		data: make(map[string]Value),
 	}
 }
 
-func (c *Cache) Set(key string, value interface{}) {
-	c.data[key] = value
+func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
+	c.data[key] = Value{
+		value, ttl, time.Now().Add(ttl),
+	}
 }
 
 func (c *Cache) Get(key string) interface{} {
-	return c.data[key]
+	curr := time.Now()
+	expr := c.data[key].expiration
+
+	if curr.Before(expr) || curr.Equal(expr) {
+		return c.data[key].value
+	} else {
+		c.Delete(key)
+		return nil
+	}
 }
 
 func (c *Cache) Delete(key string) {
